160965483a1b174694cec89a45a9cf357847ac7b
Kuldip Ottal <kuldip.ottal@gmail.com>
2023-11-28 00:02:19 +0000

    added stream task

--- etc/functions.lua
+++ etc/functions.lua
@@ -8,23 +8,26 @@ function append_converted_timestamp(tag, timestamp, record)
     end
     local newDatetime = string.format('%s-%s-%s %s:%s:%s',year, month, day, hours, minutes, seconds)
 
-    new_record = record
+    local new_record = record
     new_record["HOST_DATE_TIME"] = newDatetime
     new_record["SEQ_NO"]=record["GUID_1"] .. record["GUID_2"]
     return 2, timestamp, new_record
 end
 
 function lookup_processor(tag, timestamp, record)
-    processor_map={
-        AAABBC = "first entry,1",
-        AABCCC = "second entry,2",
-        CCCAAAA = "third entry,3",
-        DDDEEEE = "fourth entry,4",
-        ABBBBCCC = "fifth entry,5"
+    local processor_map={
+        A = "first entry,1",
+        B = "second entry,2",
+        C = "third entry,3",
+        D = "fourth entry,4",
+        E = "fifth entry,5"
     }
-    new_record = record
-    entry,number = processor_map[record["PROCESSOR"]]:match("(.+),(.+)")
+    local new_record = record
+    print(record["PROCESSOR"])
+    local entry,number = processor_map[record["PROCESSOR"]]:match("(.+),(.+)")
     new_record["LOOKUP_VALUE_ENTRY"] = entry
     new_record["LOOKUP_VALUE_NUMBER"] = number
-    return 2, timestamp, new_record
+    new_record["CURR_TIMESTAMP"] = os.date("%Y %m %d",timestamp)
+    new_record["EST_TIMESTAMP"] = os.time()
+    return 1, new_record["EST_TIMESTAMP"], new_record
 end
--- scripts/generate_json_over_tcp.py
+++ scripts/generate_json_over_tcp.py
@@ -3,13 +3,13 @@
 import socket
 import sys
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import random
 
 HOST, PORT = "localhost", 9999
 
-processor_list=["AAABBC","AABCCC","CCCAAAA","DDDEEEE","ABBBBCCC"]
+processor_list=["A","B","C","D","E"]
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,18 +21,21 @@ sock.connect((HOST, PORT))
 
 while(True):
     curr_datetime=datetime.now()
+    est_datetime=curr_datetime - timedelta(hours=5)
+
     curr_datetime_str = curr_datetime.strftime("%Y%m%d%H%M%S")
+    est_datetime_str = est_datetime.strftime("%Y%m%d%H%M%S")
          
     guid_1 = random.randint(11111, 99999)
     guid_2 = random.randint(11111, 99999)
 
     processor = random.choice(processor_list)
         
-    event={"HOST_DATE_TIME": curr_datetime_str,"GUID_1": guid_1,"GUID_2": guid_2,"PROCESSOR": processor}
+    event={"HOST_DATE_TIME": curr_datetime_str,"TRANSACTION_DATETIME": est_datetime_str,"GUID_1": guid_1,"GUID_2": guid_2,"PROCESSOR": processor}
     event_json=json.dumps(event)
     try:
         sock.sendall(bytes(event_json,encoding="utf-8"))
-        time.sleep(0.0001)
+        time.sleep(1)
     except socket.error:
         # set connection status and recreate socket
         connected = False
